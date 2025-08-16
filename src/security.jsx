5de0d6adb1b41e63b4dc530a2243b66d9377721e
caylajr2 <73130112+caylajr2@users.noreply.github.com>
2025-08-15 20:33:12 -0500

    updating

---
+++ src/security.jsx
@@ -0,0 +1,10 @@
+var client_id = '3a60e2f15bf04b9aac02bf311a731373';
+var redirect_uri = 'http://127.0.0.1:5173/';
+const scopes = [
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'user-read-email',
+  'user-read-private'
+];
+
+export { client_id, redirect_uri, scopes };
